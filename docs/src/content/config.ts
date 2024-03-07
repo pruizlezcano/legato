0a1025dcb6cb0b4b759b818ecd74f491d60c3236
Pablo Ruiz <37554617+pruizlezcano@users.noreply.github.com>
2024-03-07 19:52:12 +0100

    style: add eslint-disable-next-line comment to suppress import/prefer-default-export rule

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+// eslint-disable-next-line import/prefer-default-export
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
 };
