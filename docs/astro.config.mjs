86e8b3c17c62b0f055a843c21069cc376907495a
Pablo Ruiz <37554617+pruizlezcano@users.noreply.github.com>
2024-03-07 22:54:03 +0100

    revert: still not working
    Ref: 946ec57

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pruizlezcano.github.io',
+  base: '/legato',
   integrations: [
     starlight({
       title: 'Legato',
